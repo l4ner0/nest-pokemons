6cef29eceba998aaeb3d3e14dd048661e2fc3d46
l4ner0 <diegourbina1284@gmail.com>
2023-10-06 23:06:44 -0500

    upload

---
+++ dist/config/env.config.d.ts
@@ -0,0 +1,6 @@
+export declare const EnvConfiguration: () => {
+    environment: string;
+    mongodb: string;
+    port: string | number;
+    defaultLimit: string | number;
+};
---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MongooseModule.forRoot(process.env.MONGODB, {
+      dbName: 'pokemonsdb'
+    }),
+    PokemonModule,
+    CommonModule,
+    SeedModule,
+  ],
+})
+export class AppModule {}
